aba49edd037e89d849d3ef6aed4ee5bc808bd60b
Mizuno66 <kouta126@hotmail.co.jp>
2018-06-03 20:21:54 +0900

    bug fix Add test was_published_recentry()

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -17,3 +17,21 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days = 30)
         future_question = Question(pub_date = time)
         self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """
+        was_published_recently() returns False for quesions whose pun_date
+        is older than 1 day.
+        """
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_quesion = Question(pub_date=time)
+        self.assertIs(old_quesion.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_quesion(self):
+        """
+        was_published_recently() returns True for quesions whose pub_date
+        is within the last day.
+        """
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_quesion = Question(pub_date=time)
+        self.assertIs(recent_quesion.was_published_recently(), True)
