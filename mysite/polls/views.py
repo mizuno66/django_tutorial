3c444a0ba75d41d659125b790652a5238510ab69
Mizuno66 <kouta126@hotmail.co.jp>
2018-06-03 17:24:22 +0900

    bug fix Add result.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,HttpResponseRedirect
 #from django.template import loader
 from django.shortcuts import get_object_or_404,render
 from django.urls import reverse
@@ -43,8 +43,8 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
@@ -52,10 +52,10 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
-            'question':question,
+            'question': question,
             'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question_id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
