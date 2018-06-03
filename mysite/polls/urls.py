5aa6f689d0080c36b318220ceaeb74e033f395be
Mizuno66 <kouta126@hotmail.co.jp>
2018-06-03 18:34:31 +0900

    change use generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     #/polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     #/polls/[Integer]/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     #/polls/[Integer]/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     #/polls/[Integer]/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,50 +1,25 @@
-from django.http import HttpResponse,HttpResponseRedirect
-#from django.template import loader
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404,render
 from django.urls import reverse
+from django.views import generic
 
 from .models import *
 
-# HttpResponseを使用したケース
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#
-#     context = {
-#         'latest_question_list': latest_question_list,
-#     }
-#
-#     return HttpResponse(template.render(context, request))
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-# ショートカット(render)を使用したケース
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    def get_queryset(self):
+        """Return the last five published questions. """
+        return Question.objects.order_by('-pub_date')[:5]
 
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-    return render(request, 'polls/index.html',context)
-
-# try/exceptで処理したケース
-# def detail(request, question_id):
-#     try:
-#         question = Question.objects.get(pk=question_id)
-#     except Question.DoesNotExist:
-#         raise Http404("Question does not exist")
-#
-#     return render(request, 'polls/detail.html',{'question' : question})
-
-#ショートカット(get_object_or_404)を使用したケース
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-
-    return render(request, 'polls/detail.html',{'question' : question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
